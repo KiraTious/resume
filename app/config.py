706b7345ec2bc967aaa2f316d33d75cc15301a42
root <dragonetisatop@gmail.com>
2025-07-16 14:06:16 +0500

    в целом готовая бд+фласк в контейнерах, остался nginx

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     USER = os.environ.get('POSTGRES_USER', 'dragonet')
     PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'dragonet')
-    HOST = os.environ.get('POSTGRES_HOST', '127.0.0.1')
+    HOST = os.environ.get('POSTGRES_HOST', 'mydb')
     PORT = os.environ.get('POSTGRES_PORT', 5432)
     DB = os.environ.get('POSTGRES_DB', 'resume')
 
